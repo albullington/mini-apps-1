7024c53171007ba0e38f5439c771494e9e3388c7
Amanda <albullington@gmail.com>
2018-04-04 12:22:57 -0700

    board and squares rendering, X as value

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,16 @@
+var express = require('express');
+var app = express();
 
+app.use(express.static('public'));
 
+app.get('/', function(req, res) {
+	res.send('hello, server is getting and sending');
+});
+
+app.post('/', function(req, res) {
+	res.send('post is received');
+});
+
+app.listen(3000, () => {
+	console.log('Listening on port 3000');
+});
